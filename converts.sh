c02acf44a4c0a93725eed4ce4d6382fdaf1a8b5d
alphafalfa <cwkeller+github@pm.me>
2023-11-18 14:05:20 -0700

    more

--- converts.sh
+++ converts.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 #
+mkdir -p ./results 2>/dev/null
+mkdir -p ./processed 2>/dev/null
+
 for i in ./inputs/*.jpg; do
   ./gfpgan-ncnn-vulkan $i
-  mv result.png "./outputs/GFPGAN-$(date +%T).png"
+  mv -- "result.png" "./results/${i/%.\/inputs\///}-$(date +%T).png"
 done
 
 
+mv -v ./inputs/*.jpg ./processed/
